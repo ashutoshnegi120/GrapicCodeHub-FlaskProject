428653e8c33919c2df0c47a0ec13d44832581e58
ashutosh <ashutoshnegisgrr@gmail.com>
2022-12-01 17:55:02 +0530

    handling login

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask , render_template,request
+from flask import Flask , render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_user,LoginManager,login_required,logout_user,current_user
 import json
 
 with open("config.json",'r') as config:
@@ -15,6 +16,9 @@ else:
 db = SQLAlchemy(app)
 
 
+
+
+
 class Register(db.Model):
     SNo = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(50),nullable = False)
@@ -23,9 +27,22 @@ class Register(db.Model):
     password = db.Column(db.String(50),nullable = False)
 
 
-@app.route('/')
+@app.route('/',methods =['GET','POST'])
 def page_login():
+    if(request.method == 'POST'):
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = Register.query.filter_by(username = username).first()
+        if(user):
+            if(Register.query.filter_by(password = password).first()):
+                return redirect(url_for("Home"))
     return render_template("pages-login.html")
+
+@app.route('/home')
+def Home():
+    return render_template("home.html")
+
+
 
 @app.route('/pages-register',methods = ['GET','POST'])
 def page_register():
@@ -34,11 +51,14 @@ def page_register():
         email = request.form.get('email')
         username = request.form.get('username')
         pword = request.form.get('password')
+        if(Name != '' and email !='' and username != '' and pword !=''):
+            entry = Register(name = Name, email = email, username = username,password = pword)
+            db.session.add(entry)
+            db.session.commit()
+            return render_template("pages-register.html")
         
-        entry = Register(name = Name, email = email, username = username,password = pword)
-        db.session.add(entry)
-        db.session.commit()
     return render_template("pages-register.html")
-
+        
+        
 if __name__ =="__main__":
-    app.run()
+    app.run(debug=True)
